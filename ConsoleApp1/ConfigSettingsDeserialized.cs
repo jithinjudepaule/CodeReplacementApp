42846daba2e6589fc66f465abca1d6d4f5e386e8
jithinjudepaule <jithinjudepaule@gmail.com>
2020-07-28 09:00:52 +0530

    Initial push

---
+++ ConsoleApp1/ConfigSettingsDeserialized.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+   
+    
+
+    public class ConfigSettingsDeserialized
+    {
+        public GeneralConfig GeneralConfig { get; set; }
+        public List<ProjectConfig> ProjectConfigs { get; set; }
+
+    }
+
+    public class ProjectConfig
+    {
+        public string ProjectType { get; set; }
+        public object NotRequireFiles { get; set; }
+        public object NotRequiredDir { get; set; }
+        public object FileConfigs { get; set; }
+        public List<FileAddition> FileAdditions { get; set; }
+
+    }
+
+    public class GeneralConfig
+    {
+        public object ProjectType { get; set; }
+        public List<NotRequireFile> NotRequireFiles { get; set; }
+        public List<NotRequiredDir> NotRequiredDir { get; set; }
+        public List<FileConfig> FileConfigs { get; set; }
+        public object FileAdditions { get; set; }
+
+    }
+
+    public class FileConfig
+    {
+        public string FileExtension { get; set; }
+        public object FileName { get; set; }
+        public bool isRegex { get; set; }
+        public List<Replacer> Replacers { get; set; }
+
+    }
+
+    public class Replacer
+    {
+        public string ActualCode { get; set; }
+        public string ReplacingCode { get; set; }
+        public bool isRegex { get; set; }
+
+    }
+    public class FileAddition
+    {
+        public string FileName { get; set; }
+        public object FolderPath { get; set; }
+        public string FileRepoPath { get; set; }
+
+    }
+    public class NotRequireFile
+    {
+        public string Item1 { get; set; }
+        public string Item2 { get; set; }
+
+    }
+
+    public class NotRequiredDir
+    {
+        public string Item1 { get; set; }
+        public string Item2 { get; set; }
+
+    }
+
+  
+
+  
+
+   
+
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader jsonLoader = new StreamReader(@"..\..\..\ConfigSettings.json"))
+            {
+                string json = jsonLoader.ReadToEnd();
+
+                var myDeserializedClass = JsonConvert.DeserializeObject<ConfigSettingsDeserialized>(json);
+                ParseCsFiles(myDeserializedClass);
+            }
+        }
+
+        private static void ParseCsFiles(ConfigSettingsDeserialized myDeserializedClass)
+        {
+            var oldProjectDirectory = @"F:\MigrationProjects\WebApiToCoreMigration-master\SimpleWebApi";
+            string[] filePaths = Directory.GetFiles(oldProjectDirectory, "*.cs",
+                                         SearchOption.AllDirectories);
+            foreach (var filePath in filePaths)
+            {
+                using (StreamReader reader = new StreamReader($@"{filePath}"))
+                {
+                    string textContent = reader.ReadToEnd();
+                    string newTextContent = textContent.Replace("System.Web", "Microsoft.AspNetCore.Mvc");
+                    System.IO.File.WriteAllText(@"F:\MigrationProjects\TestFiles\HomeController.cs", newTextContent);
+
+
+
+                }
+            }
+            
+        }
+    }
+}
